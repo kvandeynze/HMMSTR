bc737cf9b40771cb4f8ddf4e4f38037754aa6809
Kinsey Van Deynze <kvandeynze@gmail.com>
2024-07-17 11:45:47 -0400

    updated conda recipe

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+chmod +x $SRC_DIR/src/c_files/test_hmm_cython.py
+cp $SRC_DIR/src/c_files/test_hmm_cython.py $PREFIX/bin
 $PYTHON setup.py clean
 $PYTHON setup.py build_ext
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
